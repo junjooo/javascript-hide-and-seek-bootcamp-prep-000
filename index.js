f8a32b12150264c4f4b3528894e27215f771aa32
Peter Choe <1peterchoe@gmail.com>
2016-12-01 11:25:12 +0000

    Done.

--- index.js
+++ index.js
@@ -13,5 +13,12 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  return document.querySelector('#grand-node div div div div')
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+  return node
 }
